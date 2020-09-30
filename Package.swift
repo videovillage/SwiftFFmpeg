6592424167f9e6a921c4bab4e40fc923375b4370
Greg Cotten <greg@gregcotten.com>
2020-09-30 13:53:43 -0700

    10.13 minimum

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftFFmpeg",
+    platforms: [
+        .macOS(.v10_13),
+    ],
     products: [
     .library(
       name: "SwiftFFmpeg",
