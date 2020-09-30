92aae97c731b32090eeb3f360a08611f5d42138a
Greg Cotten <greg@gregcotten.com>
2020-09-30 12:58:28 -0700

    point to VV CFFmpeg

--- Package.swift
+++ Package.swift
@@ -4,18 +4,20 @@
 import PackageDescription
 
 let package = Package(
-  name: "SwiftFFmpeg",
-  products: [
+    name: "SwiftFFmpeg",
+    products: [
     .library(
       name: "SwiftFFmpeg",
       targets: ["SwiftFFmpeg"]
     )
   ],
-  targets: [
-    .systemLibrary(
-      name: "CFFmpeg",
-      pkgConfig: "libavformat"
-    ),
+    dependencies: [
+        .package(
+            url: "https://github.com/videovillage/CFFmpeg.git",
+            .branch("main")
+        )
+    ],
+    targets: [
     .target(
       name: "SwiftFFmpeg",
       dependencies: ["CFFmpeg"]
